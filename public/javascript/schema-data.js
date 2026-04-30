74ba59b92f3a937820fbcc483f6bde44027f7149
Jiang <me@longtai.org>
2026-04-30 16:10:30 +0800

    fix

--- public/javascript/schema-data.js
+++ public/javascript/schema-data.js
@@ -1,24 +1,4 @@
-const personSchema = [
-  {
-    "img": "public/images/events/SITCON_2026.svg",
-    "alt": "SITCON 2026",
-    "title": "SITCON 2026",
-    "desc": "個人贊助"
-  },
-  {
-    "img": "public/images/events/SITCON_2026.svg",
-    "alt": "SITCON 2026",
-    "title": "SITCON 2026",
-    "desc": "個人贊助"
-  },
-  {
-    "img": "public/images/events/SCAICT.png",
-    "alt": "SCAICT",
-    "title": "SCAICT",
-    "subtitle": "SITCON 2026",
-    "desc": "社群攤位設備支援"
-  }
-];
+const personSchema = ;
 
 const script = document.createElement('script');
 script.type = 'application/ld+json';
